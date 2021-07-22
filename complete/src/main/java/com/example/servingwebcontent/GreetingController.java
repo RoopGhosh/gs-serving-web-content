c6adb2a5f9249b5cc25cc42e0f8f8fdf91d65004
Roop Ghosh <roop.ghosh@turbonomic.com>
2021-07-22 10:30:49 -0400

    update 239

--- complete/src/main/java/com/example/servingwebcontent/GreetingController.java
+++ complete/src/main/java/com/example/servingwebcontent/GreetingController.java
@@ -1,5 +1,13 @@
 package com.example.servingwebcontent;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,10 +16,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
-	@GetMapping("/greeting")
-	public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
-		return "greeting";
-	}
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(name = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @GetMapping("/loop100")
+    public void loop100() {
+        List<String> fixed = new ArrayList<>();
+        Executor executor = Executors.newFixedThreadPool(100);
+        for (int i = 0; i < 200; i++) {
+            //Thread.sleep(5);
+            executor.execute(() -> {
+                Random test = new Random();
+                String tss = test.doubles(100).mapToObj(GreetingController::test).collect(Collectors.joining(","));
+                fixed.add(tss);
+                if (fixed.size() > 10000) {
+                    fixed.remove(ThreadLocalRandom.current().nextInt(0, fixed.size()));
+                }
+            });
+        }
+    }
+
+    private static String test(final Double i) {
+        return i.toString();
+    }
 
 }
--- complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
+++ complete/src/main/java/com/example/servingwebcontent/ServingWebContentApplication.java
@@ -1,37 +1,13 @@
 package com.example.servingwebcontent;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.ThreadLocalRandom;
-import java.util.stream.Collectors;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
 
-    private static List<String> fixed = new ArrayList<>();
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
-        while (true) {
-            //Thread.sleep(5);
-            Random test = new Random();
-            String tss = test.doubles(100).mapToObj(ServingWebContentApplication::test).collect(Collectors.joining(","));
-            fixed.add(tss);
-            if (fixed.size() > 100) {
-                fixed.remove(ThreadLocalRandom.current().nextInt(0, fixed.size()));
-            }
-        }
-    }
-
-    private static String test(final Double i) {
-        return i.toString();
     }
 
 }
